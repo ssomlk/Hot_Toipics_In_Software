236737c88030b68ebcccde7c43cf0ca2fea00eb9
shank <shankasomasiri@gmail.com>
2018-06-11 16:08:02 +1200

    Added UML diagrams

---
+++ MainApplicationForm/MainApplicationForm/Model/Camera.cs
@@ -0,0 +1,44 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace MainApplicationForm.Model
+{
+    class Camera
+    {
+        private Capture capture;
+        private bool captureInProgress;
+        private Image<Bgr, byte> imageFrame;
+
+        public Capture getNewCapture()
+        {
+            this.capture = new Capture();
+            return this.capture;
+        }
+
+        public void setNewCapture(Capture capture)
+        {
+            this.capture = capture;
+        }
+
+        public void setCaptureInProgress(bool status)
+        {
+            this.captureInProgress = status;
+        }
+
+        public bool getCaptureInProgress()
+        {
+            return this.captureInProgress;
+        }
+
+        public Image<Bgr, byte> getFrame()
+        {
+            this.imageFrame = this.capture.QueryFrame();
+            return imageFrame;
+        }
+
+        public Image<Gray, byte> getGrayFrame(Image<Bgr, byte> image)
+        {
+            return image.Convert<Gray, byte>();
+        }
+    }
+}
